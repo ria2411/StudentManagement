339c89dae10d06fdaf3eb313017598a97e1e4602
iria_2000 <yamamotoairi@Airis-MacBook-Air.local>
2025-03-19 23:42:12 +0900

    課題１２

--- src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
@@ -1,15 +1,7 @@
 package raisetech.StudentManagement;
 
-import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import raisetech.StudentManagement.data.Student;
-import raisetech.StudentManagement.data.StudentCourses;
-import raisetech.StudentManagement.repository.StudentRepository;
-
 
 @SpringBootApplication
 public class StudentManagementApplication {
--- src/main/java/raisetech/StudentManagement/controller/StudentController.java
+++ src/main/java/raisetech/StudentManagement/controller/StudentController.java
@@ -1,40 +1,39 @@
 package raisetech.StudentManagement.controller;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import raisetech.StudentManagement.controller.converter.StudentConverter;
 import raisetech.StudentManagement.data.Student;
 import raisetech.StudentManagement.data.StudentCourses;
+import raisetech.StudentManagement.domain.StudentDetail;
 import raisetech.StudentManagement.service.StudentService;
 
 @RestController
 public class StudentController {
 
   private StudentService service;
+  private StudentConverter converter;
 
   @Autowired
-  public StudentController(StudentService service) {
+  public StudentController(StudentService service, StudentConverter converter) {
     this.service = service;
+    this.converter = converter;
   }
 
   @GetMapping("/studentList")
-  public List<Student> getStudentList() {
-    return service.searchStudentList();
+  public List<StudentDetail> getStudentList() {
+    List<Student> students = service.searchStudentList();
+    List<StudentCourses> studentCourses = service.searchStudentCoursesList();
+
+    return converter.convertStudentDetails(students, studentCourses);
   }
 
   @GetMapping("/studentCourseList")
   public List<StudentCourses> getStudentCourseList() {
     return service.searchStudentCoursesList();
-  }
-
-  @GetMapping("/studentIn30s")
-  public List<Student> getStudentsInTheir30s() {
-    return service.searchStudentsInTheir30s();
-  }
-
-  @GetMapping("/javaCourses")
-  public List<StudentCourses> getJavaCourses() {
-    return service.searchJavaCourses();
   }
 }
