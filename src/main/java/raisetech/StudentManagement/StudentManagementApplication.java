e6d09e03fdab4d42d29042018bcab5307ebc348e
iria_2000 <0570iria.unibear@gmail.com>
2025-07-11 14:44:16 +0900

    課題25

--- src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
@@ -1,11 +1,13 @@
 package raisetech.StudentManagement;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-
+@OpenAPIDefinition(info = @Info(title = "受講生情報システム"))
 @SpringBootApplication
 @MapperScan("raisetech.StudentManagement.repository")
 public class StudentManagementApplication {
