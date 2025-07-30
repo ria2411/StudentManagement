622958d893df803493b6b9a01c090cc18e907933
iria_2000 <0570iria.unibear@gmail.com>
2025-07-30 15:03:00 +0900

    ControllerValidationTest修正!

--- src/main/java/raisetech/StudentManagement/data/StudentCourses.java
+++ src/main/java/raisetech/StudentManagement/data/StudentCourses.java
@@ -2,12 +2,16 @@ package raisetech.StudentManagement.data;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Schema(description = "受講生コース情報")
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class StudentCourses {
 
   private int id;
