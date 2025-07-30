926a2328a787645d1b4292b5ba8fc4efd38b5946
iria_2000 <0570iria.unibear@gmail.com>
2025-07-30 12:03:53 +0900

    Student.java StudentCourses.javaの入力チェック

--- src/main/java/raisetech/StudentManagement/data/Student.java
+++ src/main/java/raisetech/StudentManagement/data/Student.java
@@ -1,6 +1,9 @@
 package raisetech.StudentManagement.data;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,16 +15,25 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Student {
-
   private int id;
+
+  @NotBlank(message = "名前は必須です")
   private String name;
   private String furigana;
   private String nickname;
+
+  @Email(message = "正しいメールアドレス形式で入力してください")
   private String email;
+
   private String region;
+
+  @Min(0)
   private int age;
+
   private String gender;
+
   private String remark;
+
   private boolean isDeleted;
 
 }
